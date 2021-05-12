f82d1f05bce34d0195ca673923d3da89f2f15ed0
Hrithik Khandelwal <khandelwalhrithik05@gmail.com>
2021-05-12 11:13:02 +0530

    ADD: Header.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home() {
-	return (
-		<div>
-			<h1>Home</h1>
-		</div>
-	);
+	return <Container></Container>;
 }
 
 export default Home;
+
+const Container = styled.main`
+	min-height: calc(100vh - 70px);
+	padding: 0 calc(3.5vw + 5px);
+`;
